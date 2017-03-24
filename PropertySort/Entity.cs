f18939624a552b899352ff2826bfff2dbd3e6548
keisuke kudo <melchior.balthasar.casper326@gmail.com>
2017-03-25 02:01:32 +0900

    first commit

---
+++ PropertySort/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace PropertySort {
+    class Entity {
+
+        [Sort(1)]
+        public string LastName { get; set; }
+
+        [Sort(0)]
+        public string FirstName { get; set; }
+
+        [Sort(2)]
+        public int Age { get; set; }
+    }
+}
--- PropertySort/Program.cs
+++ PropertySort/Program.cs
@@ -2,11 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PropertySort {
     class Program {
         static void Main(string[] args) {
+
+            //出力する内容
+            var entitys = new List<Entity>() {
+                new Entity() { LastName = "Ono", FirstName = "Shinji", Age = 37 },
+                new Entity() { LastName = "Honda", FirstName = "Keisuke",  Age = 30 },
+                new Entity() { LastName = "Nakayama", FirstName = "Masashi", Age = 49 },
+            };
+
+
+            var csv = new ConvertCsv<Entity>() { Source = entitys };
+
+            Console.WriteLine(csv.ToCsv());
         }
     }
+
+    /// <summary>
+    /// List<T>型のオブジェクトをCSV形式の文字列に変換する
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ConvertCsv<T> {
+
+        private List<T> Entitys;
+
+        /// <summary>
+        /// 変換対象ソース
+        /// </summary>
+        public List<T> Source {
+            set {
+                this.Entitys = value;
+            }
+        }
+
+        /// <summary>
+        /// CSV形式で出力
+        /// </summary>
+        /// <returns></returns>
+        public string ToCsv() {
+
+            var entityNames = typeof(T).GetProperties().
+                                        //SortAttribute属性が設定されているプロパティのみ対象
+                                        Where(e => Attribute.IsDefined(e, typeof(SortAttribute))).
+                                        //SortAttribute属性のSortIndexプロパティでソート
+                                        OrderBy(e => ((SortAttribute)Attribute.GetCustomAttribute(e, typeof(SortAttribute))).SortIndex).
+                                        //プロパティ名を取得
+                                        Select(e => e.Name);
+
+            var result = new StringBuilder();
+
+            //ヘッダー出力
+            result.AppendLine(string.Join(",", entityNames));
+
+            foreach (var entity in this.Entitys) {
+                //要素出力
+                var element = entityNames.Select(n => typeof(T).GetProperty(n).GetValue(entity));
+                result.AppendLine(string.Join(",", element));
+            }
+
+            return result.ToString();
+        }
+    }
+
 }
